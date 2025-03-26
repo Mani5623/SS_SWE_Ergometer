cb058ad4a2f993a1e16d47e5c186d27c022ffcb3
Mani5623 <hm9703@mci4me.at>
2025-03-26 12:42:16 +0100

    Pandas installation

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ from my_functions import build_experiment, build_person
 
 if __name__ == "__main__":
 
-    supervisorr = "Manuel Hager"
+    supervisor = "Manuel Hager"
     subject = build_person("Max","Mustermann","male",20)
-    experiment = build_experiment("Test","18-03-20225",supervisorr,subject)
+    experiment = build_experiment("Test","18-03-20225",supervisor,subject)
     
     print(experiment)
-    
