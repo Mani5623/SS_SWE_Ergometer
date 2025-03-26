fa1a5fa6097251d21a4b417ce85e830977555b47
Mani5623 <hm9703@mci4me.at>
2025-03-26 11:59:15 +0100

    test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from my_functions import build_experiment, build_person
 
 if __name__ == "__main__":
 
-    supervisor = build_person("Manuel","Hager","male",20)
+    supervisor = "Manuel Hager"
     subject = build_person("Max","Mustermann","male",20)
     experiment = build_experiment("Test","18-03-20225",supervisor,subject)
     
