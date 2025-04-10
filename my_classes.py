9d4a5491d80851fe1777a6ffdc0d531e4819c639
Mani5623 <hm9703@mci4me.at>
2025-04-10 11:26:04 +0200

    Aufgabe62

--- my_classes.py
+++ my_classes.py
@@ -1,25 +1,44 @@
-class Subject():
-    def __init__(self, first_name, last_name, sex, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.sex = sex
-        self.age = age
-        self.max_hr = None
+from datetime import datetime
 
-    def estimate_max_hr(self):
-        """A function that estimates the maximum heart rate of a subject"""
-        if self.sex == "male":
-            self.max_hr = 220 - self.age
-        elif self.sex == "female":
-            self.max_hr = 226 - self.age
-        return self.max_hr
-
-class Supervisor():
+class Person:
     def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
 
-class Experiment():
+class Subject(Person):
+    def __init__(self, first_name, last_name, sex, birthdate):
+        super().__init__(first_name, last_name)
+        self.sex = sex
+        self.__birthdate = birthdate  # Privates Attribut
+        self.max_hr = None
+        self.age = self._calculate_age()
+
+    def _calculate_age(self):
+        """Berechnet das Alter basierend auf dem Geburtsdatum"""
+        today = datetime.now().date()
+        born = self.__birthdate.date() if isinstance(self.__birthdate, datetime) else self.__birthdate
+        
+        age = today.year - born.year
+        
+        # Korrektur, falls der Geburtstag in diesem Jahr noch nicht erreicht wurde
+        if today < datetime(today.year, born.month, born.day).date():
+            age -= 1
+            
+        return age
+
+    def estimate_max_hr(self):
+        """Berechnet die maximale Herzfrequenz basierend auf dem Alter"""
+        if self.sex == "male":
+            self.max_hr = 223 - 0.9 * self.age
+        elif self.sex == "female":
+            self.max_hr = 226 - 1.0 * self.age
+        return self.max_hr
+
+class Supervisor(Person):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+
+class Experiment:
     def __init__(self, name, date):
         self.name = name
         self.date = date
@@ -33,4 +52,5 @@ class Experiment():
         self.supervisor = supervisor
         
     def __str__(self):
-        return f"Experiment: {self.name}, Date: {self.date}, Subject: {self.subject.first_name} {self.subject.last_name}, Supervisor: {self.supervisor.first_name} {self.supervisor.last_name}"
+        subject_info = f"Subject: {self.subject.first_name} {self.subject.last_name}, Sex: {self.subject.sex}, Age: {self.subject.age}"
+        return f"Experiment: {self.name}, Date: {self.date}, {subject_info}, Supervisor: {self.supervisor.first_name} {self.supervisor.last_name}"
--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 from my_classes import Subject, Supervisor, Experiment
+from datetime import datetime
 
 if __name__ == "__main__":
-
     # Erstellen eines Leistungstests
     supervisor = Supervisor("Manuel", "Hager")
-    subject = Subject("Max", "Mustermann", "female", 30)
+    
+    # Geburtsdatum als datetime-Objekt anstelle des Alters
+    birthdate = datetime(2004, 6, 3) 
+    subject = Subject("Max", "Mustermann", "male", birthdate)
     subject.estimate_max_hr()
 
     experiment = Experiment("Leistungstest", "2021-01-01")
     experiment.add_subject(subject)
     experiment.add_supervisor(supervisor)
-
 
     print(experiment)
